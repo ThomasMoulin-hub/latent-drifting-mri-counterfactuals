524eae6f7a489c9752002f98792de45385da9980
Thomas Moulin <thomas.moulin@telecom-paris.fr>
2026-05-03 22:01:24 -0400

    move OASISClassifier import and serialization to the correct location in train.py

--- src/train.py
+++ src/train.py
@@ -10,13 +10,14 @@ import lightning as L
 import rootutils
 import torch
 
-from src.models.classifier import OASISClassifier
-torch.serialization.add_safe_globals([OASISClassifier])
 from lightning import Callback, LightningDataModule, LightningModule, Trainer
 from lightning.pytorch.loggers import Logger
 from omegaconf import DictConfig
 
 rootutils.setup_root(__file__, indicator=".project-root", pythonpath=True)
+
+from src.models.classifier import OASISClassifier
+torch.serialization.add_safe_globals([OASISClassifier])
 # ------------------------------------------------------------------------------------ #
 # the setup_root above is equivalent to:
 # - adding project root dir to PYTHONPATH
